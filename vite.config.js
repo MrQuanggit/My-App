bf35abdd8320ed8fc840c7177055c5a6056fed3e
QuangVo <vodangquang13579@gmail.com>
2025-11-12 15:15:35 +0700

    setting_tailwind

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,11 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
-});
+    server: {
+        host: true,
+        port: 5173,
+        hmr: {
+            host: 'localhost',
+        },
+    },
+})
